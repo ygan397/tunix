4b1c6e186938aafa7a2a7aa7e15695f06bff07c6
lc5211 <linchai@google.com>
2026-01-15 20:49:15 +0000

    remove wandb key

--- examples/deepscaler/train_deepscaler_nb.py
+++ examples/deepscaler/train_deepscaler_nb.py
@@ -36,7 +36,7 @@ pathwaysutils.initialize()
 print("jax devices: ", jax.devices())
 
 try:
-  wandb.login(key="e27080071466d108dc7c16fc6ff885b296d8b608")
+  wandb.login(key="")
   print("linchai: logged in to W&B")
 except wandb.errors.UsageError as e:
   print(f"Failed to log in to W&B: {e}")
